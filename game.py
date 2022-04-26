f0d0c2f59ac53d5d1057e6d18e1a506069a5a065
Kendall Morgan <kuyukip@gmail.com>
2022-04-26 17:34:28 -0500

    began making classes and started testing just the printing of the initial instructions

---
+++ game.py
@@ -0,0 +1,32 @@
+
+from time import sleep
+
+class Game:
+    def __init__(self):
+        return
+    
+    def run_game(self):
+        print('Welcome to RPSLS!')
+        sleep(0.5)
+        print('')
+        print('This is a best two out of three.')
+        print('May the best player win.')
+        print('')
+        print('Rock crushes Scissors')
+        sleep(0.5)
+        print('Paper covers Rock')
+        sleep(0.5)
+        print('Rock crushes Lizard')
+        sleep(0.5)
+        print('Lizard poisons Spock')
+        sleep(0.5)
+        print('Spock smashes Scissors')
+        sleep(0.5)
+        print('Scissors decapitates Lizard')
+        sleep(0.5)
+        print('Lizard eats Paper')
+        sleep(0.5)
+        print('Paper disproves Spock')
+        sleep(0.5)
+        print('Spock vaporizes Rock')
+
